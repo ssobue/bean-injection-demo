9c900949015896260c802b2c78ec68347d40924e
SOBUE Sho <sho@sobue.name>
2022-12-04 22:43:10 +0900

    Spring Boot 3.0.0

--- src/test/java/jp/sobue/BeanInjectionDemoApplicationTests.java
+++ src/test/java/jp/sobue/BeanInjectionDemoApplicationTests.java
@@ -1,20 +1,17 @@
 package jp.sobue;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Application Load Test.
  *
  * @author ssobue
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class BeanInjectionDemoApplicationTests {
+class BeanInjectionDemoApplicationTests {
 
   /** Load Application Context. */
   @Test
-  public void contextLoads() {}
+  void contextLoads() {}
 }
